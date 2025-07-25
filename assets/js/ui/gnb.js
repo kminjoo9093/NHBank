3e49756faadf6c24b1ef5b5e90bc882bb757cf9e
kminjoo9093 <kminjoo9093@naver.com>
2025-07-25 12:29:13 +0900

    메가메뉴 메뉴리스트 패딩값 수정

--- assets/js/ui/gnb.js
+++ assets/js/ui/gnb.js
@@ -1,11 +1,9 @@
 import { activeBackdrop, hideBackdrop } from "./handleBackdrop.js";
-
 import { handleExpanded } from "./handleExpanded.js";
 
 export const gnb = () => {
   handleExpanded(".gnb .nav__list .btn--more", { isGnb: true });
 };
-
 
 export const tabFocusNav = () => {
   const mainNavs = document.querySelectorAll(".gnb__list > li > a");
